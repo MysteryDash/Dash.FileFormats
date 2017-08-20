88d5475baccf5ee60738bfefe5df60ea63eb22cf
Alexandre Quoniou <alexandre.quoniou@protonmail.com>
2017-08-20 18:25:04 +0200

    First commit. This repository may or may not be merged in the future with the File Formats library.

---
+++ Dash.FileFormats.VDF/UnexpectedCharacterException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Dash.FileFormats.VDF
+{
+    public class UnexpectedCharacterException : Exception
+    {
+        public char Character { get; }
+        public int Index { get; }
+
+        public UnexpectedCharacterException()
+        {
+
+        }
+
+        public UnexpectedCharacterException(char character) : this(character, -1)
+        {
+            
+        }
+
+        public UnexpectedCharacterException(char character, int index)
+        {
+            Character = character;
+            Index = index;
+        }
+
+        public UnexpectedCharacterException(string message) : base(message)
+        {
+
+        }
+
+        public UnexpectedCharacterException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
