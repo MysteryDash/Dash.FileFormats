575c6444d901676ef567dd00764382d2a37fabd3
Alexandre Quoniou <alexandre.quoniou@protonmail.com>
2017-08-31 18:02:46 +0200

    Added missing license headers. Added CONTRACTS_FULL to release build.

--- Dash/FileFormats/FormatException.cs
+++ Dash/FileFormats/FormatException.cs
@@ -1,7 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// 
+// This file is licensed under the terms of the Simple Non Code License (SNCL) 2.1.0.
+// The full license text can be found in the file named License.txt.
+// Written originally by Alexandre Quoniou in 2016.
+//
+
+using System;
 using System.Runtime.Serialization;
-using System.Text;
 
 namespace Dash.FileFormats
 {
--- Dash/FileFormats/VersionException.cs
+++ Dash/FileFormats/VersionException.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// 
+// This file is licensed under the terms of the Simple Non Code License (SNCL) 2.1.0.
+// The full license text can be found in the file named License.txt.
+// Written originally by Alexandre Quoniou in 2016.
+//
+
+using System;
 using System.Runtime.Serialization;
 
 namespace Dash.FileFormats
