46a4b2ca8c481a6af8e3fd4ab338d71e2169a1c3
Alexandre Quoniou <alexandre.quoniou@protonmail.com>
2017-09-01 15:27:00 +0200

    Added Unit Tests in another solution.

---
+++ Dash.Tests/Dash.Tests/FileFormats/Valve/VDF/VdfConvertTests.cs
@@ -0,0 +1,50 @@
+﻿// 
+// This file is licensed under the terms of the Simple Non Code License (SNCL) 2.1.0.
+// The full license text can be found in the file named License.txt.
+// Written originally by Alexandre Quoniou in 2017.
+//
+
+using System.IO;
+using Xunit;
+
+namespace Dash.FileFormats.Valve.VDF
+{
+    public class VdfConvertTests
+    {
+        private string BasePath { get; } = @"Samples\VDF\";
+
+        [Theory]
+        [InlineData("KSP.acf", true)]
+        [InlineData("KSP_OneLine.acf", true)]
+        [InlineData("KSP_NoQuotes.acf", true)]
+        public void Deserialize_File(string filename, bool shouldWork)
+        {
+            try
+            {
+                var path = Path.Combine(BasePath, filename);
+                VdfConvert.Deserialize(File.ReadAllText(path));
+            }
+            catch
+            {
+                if (shouldWork)
+                    throw;
+            }
+        }
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", true)]
+        public void Deserialize_Inline(string data, bool shouldWork)
+        {
+            try
+            {
+                VdfConvert.Deserialize(data);
+            }
+            catch
+            {
+                if (shouldWork)
+                    throw;
+            }
+        }
+    }
+}
---
+++ Dash.Tests/Dash.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Dash.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dash.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6557e10f-0743-4183-bcb0-d15385a7b6bc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
